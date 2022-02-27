19194842b5da9b5bc1715ffa5739163f259eb56b
IdVbI <95322898+IdVbI@users.noreply.github.com>
2022-02-27 14:05:38 +0900

    Add files via upload

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 임성빈 on 2022/02/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
+    }
+}
---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,46 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 임성빈 on 2022/02/06.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filterLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filterLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
